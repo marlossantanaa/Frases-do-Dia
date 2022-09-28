07384b93019d68f114f3a69b93fcc2393e62d8ab
Marlos Santana <83357882+marlossantanaa@users.noreply.github.com>
2022-09-28 12:25:08 -0300

    Finalizando o App frases do Dia.

--- lib/main.dart
+++ lib/main.dart
@@ -8,57 +8,61 @@ void main(){
   ));
 }
 class Home extends StatefulWidget {
-  const Home({Key? key}) : super(key: key);
-
   @override
-  State<Home> createState() => _HomeState();
+  _HomeState createState() => _HomeState();
 }
 
 class _HomeState extends State<Home> {
-  var _frases=[
-'Tudo o que um sonho precisa para ser realizado é alguém que acredite que ele possa ser realizado.'
-'Imagine uma nova história para sua vida e acredite nela.    '
-'Nunca deixe ninguém te dizer que não pode fazer alguma coisa. Se você tem um sonho tem que correr atrás dele. As pessoas não conseguem vencer e dizem que você também não vai vencer. Se você quer uma coisa corre atrás.'
-'Não existe um caminho para a felicidade. A felicidade é o caminho.'
-'Não espere por uma crise para descobrir o que é importante em sua vida.'
+
+  var _frases = [
+    "Sou apenas um pequeno planeta que se perde diariamente em todo o seu universo.",
+    "Novas amizades serão sempre bem-vindas para darem cor e alegria ao meu dia a dia.",
+    "Gratidão não é pagamento, mas um reconhecimento que se demonstra no dia a dia.",
+    "Nem toda mudança importante acontece de repente e faz barulho, algumas são silenciosas e vão se fazendo no dia a dia."
+  "Enfim, o fundo do poço da vergonha foi atingido quando a informática, o marketing, o design, a publicidade, todas as disciplinas da comunicação apoderaram-se da própria palavra conceito e disseram: é nosso negócio, somos nós os criativos, nós somos os 'conceituadores'!",
+    "O maior problema em informatica não é o hardware ou o software, quase sempre está localizado entre a cadeira e o monitor",
+    "Todos já fomos (e na verdade ainda somos) a moça ou o rapaz da informática.",
+    "Todo profissional de TI já ouviu a frase “fulano, a internet caiu, você consegue resolver?”. ",
+    "É claro que nossa aparência de calmo e tranquilo nem sempre transmite exatamente o que estamos pensando ou desejando poder fazer. Mas nada como manter a classe, mesmo em situações adversas. "
+
   ];
+
   var _frasesGerada='Clique abaixo para gerar uma frase';
 
 void _gerarFrase(){
-  var numeroSorteado= Random().nextInt(_frases.length);
+  var _numeroSorteado= Random().nextInt(_frases.length);
   setState(() {
-    _frasesGerada=_frasesGerada[numeroSorteado];
+    _frasesGerada= _frases[_numeroSorteado];
   });
+
 }
 
-
-
-  @override
+@override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+        centerTitle: true,
         title: Text('Frases do dia'),
-        backgroundColor: Colors.purple,
+        backgroundColor: Colors.lightBlue ,
 
       ),
-      body:Container(
+      body: Center(
+      child:Container(
         padding: EdgeInsets.all(16),
-        width: double.infinity,
-        decoration: BoxDecoration(
-          border: Border.all(width: 3,color: Colors.amber)
 
-        ),child:Column(
+        child:Column(
     mainAxisAlignment: MainAxisAlignment.spaceEvenly,
         crossAxisAlignment: CrossAxisAlignment.center,
         children: <Widget>[
 
-Image.asset("images/logo.png"),
+Image.asset("images/logo1.png"),
           Text(
-            'Clique Abaixo para gerar uma frase',
+            _frasesGerada,
             textAlign: TextAlign.justify,
             style: TextStyle(
-              fontSize: 18,
+              fontSize: 25,
               fontStyle: FontStyle.normal,
+                fontWeight: FontWeight.bold,
               color: Colors.black
             ),
           ),
@@ -72,13 +76,13 @@ Image.asset("images/logo.png"),
 
                    ),
          ),
+
            onPressed: _gerarFrase,
          )
         ],
       )
       ) ,
-
-
+    )
     );
   }
 }
